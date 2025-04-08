0bbcee2cdc3359549a11eee9fa1c7a5f16e808da
Chinonso Orekie <qutesly@gmail.com>
2025-04-08 15:57:10 +0100

    first commit

--- src/components/Footer/Footer-component.jsx
+++ src/components/Footer/Footer-component.jsx
@@ -2,17 +2,40 @@ import React from "react";
 
 const FooterComponent = () => {
   return (
-    <div className="w-full border relative text-white">
-      <div className="flex flex-col items-start gap-[19px] justify-between px-10 md:px-40 py-10">
-        <img src="./images/Edoubleone-logo-1.png" alt="" />
-        <p className="w-[311px]">
-          Edoublone - the leading digital agency based in the Us, working with
-          top-tier clients, from start-ups to enterprises.
-        </p>
+    <div className="w-full  relative text-white pb-32">
+      <div className="w-full border flex items-start gap-[19px] justify-between px-10 md:px-40 py-10">
+        <div className="flex flex-col items-start gap-[19px] justify-between">
+          <img src="./images/Edoubleone-logo-1.png" alt="" />
+          <p className="w-[311px]">
+            Edoublone - the leading digital agency based in the Us, working with
+            top-tier clients, from start-ups to enterprises.
+          </p>
+        </div>
+        <div className="flex flex-col items-start gap-8">
+          <div className="flex gap-[20px]">
+            <img src="./images/message.png" alt="" />
+            <p>support@edoubleone.net</p>
+          </div>
+          <div className="flex gap-[20px]">
+            <img src="./images/message.png" alt="" />
+            <p>support@edoubleone.net</p>
+          </div>
+          <div className="flex gap-[20px]">
+            <img src="./images/message.png" alt="" />
+            <p>support@edoubleone.net</p>
+          </div>
+          <div className="flex gap-[20px]">
+            <img src="./images/facebook.png" alt="" />
+            <img src="./images/instagram.png" alt="" />
+            <img src="./images/LinkedIn.png" alt="" />
+            <img src="./images/socials.png" alt="" />
+          </div>
+        </div>
       </div>
 
-      <div className="bottom-[0] left-[0] -z-10">
-        <img src="./images/Footer-Background-Pattern.png" alt="" />
+      <div className="w-full absolute bottom-[0] left-[0] -z-10 -mt-10">
+        <img className="border" src="./images/Footer-Background-Pattern.png" alt="" />
+        {/* <img src="./images/stars.png" alt="" /> */}
       </div>
     </div>
   );
